b2e3c9764f0115d0158a12de8630260286a78a7c
sebasR26 <sebassrp47@gmail.com>
2026-08-11 20:50:22 -0500

    Creacion de la clase objetual

---
+++ objMatriz.java
@@ -0,0 +1,29 @@
+public class objMatriz {
+    int dato;
+
+    
+
+    public objMatriz(int dato) {
+        this.dato = dato;
+    }
+
+
+
+    public objMatriz() {
+    }
+
+
+
+    public int getDato() {
+        return dato;
+    }
+
+
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+    
+    
+
+}
